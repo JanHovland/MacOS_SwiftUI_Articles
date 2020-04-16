f6da29a07bd5ac4c241460bf01a512fc49be4655
JanHovland <jan.hovland@lyse.net>
2020-04-16 18:47:45 +0200

    Initial Commit

---
+++ MacOS_SwiftUI_Articles/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MacOS_SwiftUI_Articles
+//
+//  Created by Jan Hovland on 16/04/2020.
+//  Copyright © 2020 Jan Hovland. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
